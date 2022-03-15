3ecbc57a9e326ec5f0bda1b1cee8b1a47ad64635
holabayor <liasu.olabayo@gmail.com>
2022-03-15 22:59:22 +0000

    ready to go

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @a : input integer a
+ * @b :the input integer b
+ * Return: the sum
+ */
+int add(int a, int b)
+{
+	_putchar((a + b) + '0');
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - entry point
+ * @n: Takes an input n
+ * Return: list of number to 98 from n (ascending or descending)
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = 98; i <= n; i++)
+			printf('%d ', i);
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+			printf('%d,', i);
+	}
+}
