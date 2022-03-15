65b888f97694dff14084b51a2ae15f89066a9f38
holabayor <liasu.olabayo@gmail.com>
2022-03-15 23:42:12 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,22 @@
  */
 void times_table(void)
 {
-	int i, j, res;
+	int i, res;
 
 	for (i = 0; i <= 9; i++)
 	{
+		int j;
 
-		for (j = 0; j <= 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			res = i * j;
-			_putchar(res + '0');
-			_putchar(',');
-			_putchar(' ');
+			_putchar((res / 10) + '0');
+			_putchar((res % 10) + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
