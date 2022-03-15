0899d91eb365fdf46b127fa8622cd32a3d11c2c2
holabayor <liasu.olabayo@gmail.com>
2022-03-15 18:19:23 +0000

    Jacker Bauer this

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 0; i <= 24; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 59; j++)
+		{
+			printf("%i:%i\n", i, j);
+		}
+	}
+	return (0);
+}
