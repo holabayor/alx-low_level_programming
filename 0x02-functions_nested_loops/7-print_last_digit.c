17bacde4a0634c2443cbff8e60dfcdbf30a0e58a
holabayor <liasu.olabayo@gmail.com>
2022-03-15 07:30:24 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - return the absolute value of an integer
+ * @val: - the input value
+ * Return: last value of the digit
+ */
+int print_last_digit(int val)
+{
+	int digit = val % 10
+	return (val);
+}
