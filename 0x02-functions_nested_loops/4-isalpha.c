7f03550555ab26ec6531c159cf773ca44c3bb0bc
holabayor <liasu.olabayo@gmail.com>
2022-03-15 06:13:31 -0700

    fixed task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _is_alpha- checks if a character is an alphabet or not
+ * _isalpha- checks if a character is an alphabet or not
  * @c: is the input character
  * Return: 1 if success
  */
@@ -9,7 +9,7 @@ int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else if (c >= 65 && c<= 90
+	else if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
