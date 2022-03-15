cbbd0921ed4eb2b162d616564908b23ac6d0efba
holabayor <liasu.olabayo@gmail.com>
2022-03-15 05:53:55 -0700

    first task 3 trial

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase character or not
+ *
+ * Return: 1 if success
+ */
+int _islower(int c)
+{
+	if c >= 97 && c <= 122;
+		_putchar(1);
+	else
+		_putchar(0);
+}
