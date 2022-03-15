7ecb6de261fc47c7ec6e8ad003f0df3966fb2e6b
holabayor <liasu.olabayo@gmail.com>
2022-03-15 23:10:00 +0000

    ready to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,12 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		for (i = 98; i <= n; i++)
-			printf('%d ', i);
+			printf("%d, ", i);
 	}
 	else
 	{
 		for (i = n; i <= 98; i++)
-			printf('%d,', i);
+			printf("%d, ", i);
 	}
+	printf("\n");
 }
