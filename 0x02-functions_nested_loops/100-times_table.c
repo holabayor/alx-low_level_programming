752db6f59d6817dbd78bb0986171119ded0e3914
holabayor <liasu.olabayo@gmail.com>
2022-03-16 11:28:22 +0000

    add times table with input final fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,6 +9,7 @@ void print_times_table(int size)
 	if ((size >= 0) && (size <= 15))
 	{
 		int row, col = size, res;
+		
 		for (row = 0; row <= size; row++)
 		{
 			for (col = 0; col <= size; col++)
