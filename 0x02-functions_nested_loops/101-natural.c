e1eac5306e40f355182d643b5a64494132f20b55
holabayor <liasu.olabayo@gmail.com>
2022-03-16 11:43:24 +0000

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,9 +6,9 @@
  */
 void print_natural(void)
 {
-	int sum = 0;
+	int i, sum = 0;
 
-	for (int i = 0; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
