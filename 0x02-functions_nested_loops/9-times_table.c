7552ad38bfb1105b35ec09137a74201913f670a7
holabayor <liasu.olabayo@gmail.com>
2022-03-16 10:34:35 +0000

    add times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			res = i * j;
-			if ((res == 0) && (i == 0))
+			if ((res == 0) && (j == 0))
 				_putchar(res + '0');
 			else if (res <= 9)
 			{
