8435f213bb73eb75ed02dc4e4aed62a34e3a4008
holabayor <liasu.olabayo@gmail.com>
2022-03-16 19:51:02 +0000

    task 2 last fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+	largest = (a > b) ? ((b > c) ? a : c) : ((b > c) ? b : c)
 
 	return (largest);
 }
