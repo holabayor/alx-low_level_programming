8baac3e85b8d0dac7fae4a5869e38f0f30350d22
holabayor <liasu.olabayo@gmail.com>
2022-03-17 11:59:20 +0000

    task 6 fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,9 +12,11 @@ void print_line(int n)
 	if (n <= 0)
 		_putchar('\n');
 
-	while (n > 0 && i < n)
+	else
 	{
-		_putchar('_');
-		i++;
+		for (i = 1; i <= n, i++)
+			_putchar('_');
+			i++;
+		_putchar('\n');
 	}
 }
