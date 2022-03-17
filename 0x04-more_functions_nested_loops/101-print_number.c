7b531e03c24e5343bec8e62390a36a44e47e0947
holabayor <liasu.olabayo@gmail.com>
2022-03-17 14:16:19 +0000

    love this

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - print out a number
+ * @n: the number to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	while (n > 9)
+	{
+		i = n % 10;
+		_putchar(i + '0');
+		n /= 10;
+	}
+	_putchar(n + '0');
+}
