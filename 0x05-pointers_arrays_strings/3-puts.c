34adc58a0149ad443f43ea64299e55b6e003c0fb
holabayor <liasu.olabayo@gmail.com>
2022-03-21 21:18:31 +0000

    Task three :fire: :fire:

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @s: input string
+ * Return: void
+ */
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(i)
+		i++;
+	_putchar('\n');
+}
