4d58010a8213c58e4533d811c6cbabfaf2ce83eb
holabayor <liasu.olabayo@gmail.com>
2022-03-21 21:27:35 +0000

    Task four :fire: :fire:

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: input string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	while (i--)
+		_putchar('\n');
+	_putchar('\n');
+}
