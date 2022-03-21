470215d7e3b00894366a4c356a3932b44c5f70c3
holabayor <liasu.olabayo@gmail.com>
2022-03-21 22:51:59 +0000

    Tasks 6 and 7:fire::rocket::fire:

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,7 @@ void puts2(char *str)
 	int i = 0;
 
 	while (str[i])
-	{
-		
+	{		
 		if (i % 2 == 0)
 			_putchar(str[i]);
 		i++;
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - print the secong half of a string
+ * @str: pointer to the input string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0, i;
+
+	while (str[len])
+		len++;
+
+	for (i = (len - 1) /2; i <= len, i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
