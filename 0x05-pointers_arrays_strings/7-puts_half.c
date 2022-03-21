fa8620585b12ed3478569f3609ec5826b07c4918
holabayor <liasu.olabayo@gmail.com>
2022-03-21 23:06:15 +0000

    Tasks 6 and 7:fire::rocket::fire:

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,8 +13,7 @@ void puts_half(char *str)
 	while (str[len])
 		len++;
 
-	i = len % 2 ? (len / 2) : (len - 1 / 2);
-	for (i; i <= len; i++)
+	for (i = (len % 2 ? (len / 2) : (len - 1 / 2)); i <= len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
