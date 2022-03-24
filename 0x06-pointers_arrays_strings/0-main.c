414892bcdb567010499587d1b265c4d114b36b56
holabayor <liasu.olabayo@gmail.com>
2022-03-24 19:57:08 +0000

    :fire:

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "world\n";
+	char *ptr;
+
+	printf("%s/n", s1);
+	ptr = _strcat(s1, s2);
+	return(0);
+}
