f297da11be591c99003cd05c6439c9e08549317f
holabayor <39333226+holabayor@users.noreply.github.com>
2022-03-24 21:45:20 +0100

    update task 0 :fire:

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates two strings
  * @dest: destination string
@@ -8,14 +7,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	while (*dest)
-		*dest++;
-	while (*src)
-	{
-		*dest = *src;
-		*src++;
-		*dest++;
-	}
-	*dest = '\0';
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		for (j = 0; src[j] != '\0'; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+	dest[i] = '\0';
 	return (dest);
 }
