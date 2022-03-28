981f4c7c4f20fe450cef7c8f928e5a679a9c49ce
holabayor <liasu.olabayo@gmail.com>
2022-03-28 22:55:21 +0000

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,8 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, count = 0;
+	int i, j;
+	unsigned int count = 0;
 
 	for (i = 0; *(accept + i) != '\0'; i++)
 		for (j = 0; *(s + j) != '\0'; j++)
