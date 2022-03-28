dfd5941e45af6b1491f6355558ecb737ea7b3e35
holabayor <liasu.olabayo@gmail.com>
2022-03-28 21:16:59 +0000

    let's go

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: memory area to be filled
+ * @c: the length of the byte to be filled
+ * Return: pointer to the memory area
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+		if *(s + i) = c:
+			return (*s);
+	return ('\n');
+}
