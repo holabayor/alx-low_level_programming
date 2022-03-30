8dec33ba762d173da9c0e2839ae24d82e129a564
holabayor <liasu.olabayo@gmail.com>
2022-03-30 02:00:38 -0700

    check here

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a line
+ * @s: input string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar(s);
+	else
+		_putchar(*s);
+		_puts_recursion(s + 1);
+}
