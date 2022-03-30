f5b7b333b15437238517e948624b4c30f217694c
holabayor <liasu.olabayo@gmail.com>
2022-03-30 16:44:45 +0000

    square root fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,28 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt - returns the natural square root of a number
  * @n: value to find its square
- * Return: square root 
+ * @val: bound
+ * Return: square root
  */
-int _sqrt_recursion(int n)
+int _sqrt(int n, int val)
 {
-	int val = 1;
-
 	if (n < 0)
 		return (-1);
 	if (val * val == n)
 		return (val);
 	else if (val * val < n)
-	{
-		val += 1;
-		return (_sqrt_recursion(n));
-	}
+		return (_sqrt(n, val + 1));
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: value to find its square
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
 }
