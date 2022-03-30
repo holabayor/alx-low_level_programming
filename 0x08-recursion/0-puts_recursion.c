27a231fcc866e0bcb8ad19ee476a56c175b3e749
holabayor <liasu.olabayo@gmail.com>
2022-03-30 02:13:30 -0700

    task 0 with a bang :fire:

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar(s);
+		_putchar('\n');
 	else
-		_putchar(s);
-		_puts_recursion(*(s + 1));
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
