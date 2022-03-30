e92aa609f7b7f1537ba5e8250acae7acfedfd823
holabayor <liasu.olabayo@gmail.com>
2022-03-30 02:24:37 -0700

    with another bang:fire:

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	_puts_recursion("Puts with recursion");
+	_print_rev_recursion("\nPuts with recursion");
 	return (0);
 }
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev__recursion - print a line
+ * @s: input string
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
