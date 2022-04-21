f2d59b084ed8422676d7a7e87e34dd68fca9ac1b
holabayor <liasu.olabayo@gmail.com>
2022-04-21 22:59:28 +0100

    First commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - function to print a list
+ * @h: the head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
