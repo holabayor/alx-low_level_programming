8e4f748183b9775094f9e079851c9fcf5d54e0ce
holabayor <39333226+holabayor@users.noreply.github.com>
2022-04-27 23:00:18 +0100

    last dance

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,31 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at an index
- * @head: head of the linked list
- * @idx: index of the new value
- * @n: integer value to store in the list.
- * Return: address of the head.
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *tmp;
-	size_t i;
+	unsigned int i;
+	listint_t *new;
+	listint_t *h;
+
+	h = *head;
+
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			h = h->next;
+		}
+	}
+
+	if (h == NULL && idx != 0)
+		return (NULL);
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
-	tmp = *head;
-	
-	for (i = 0; i < idx - 1; i++)
-	{
-		tmp = tmp->next;
-	}
-	tmp->next = new->next;
 	new->n = n;
-	new->next = tmp;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
 
 	return (new);
 }
