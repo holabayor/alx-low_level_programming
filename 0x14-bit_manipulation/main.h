c4568cd488bab235b3927a6bff672e9f072cfee7
holabayor <liasu.olabayo@gmail.com>
2022-04-28 20:49:13 +0100

    fixed header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,11 @@
 #include <string.h>
 #include <stdio.h>
 
+int _putchar(char c)
+{
+    return ((write(1, &c, 1));
+}
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
