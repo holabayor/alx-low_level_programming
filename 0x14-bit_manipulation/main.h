a7f1812afcb56f41c2b2daf48eb442d40c03c953
holabayor <liasu.olabayo@gmail.com>
2022-04-29 13:21:42 +0100

    update header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,16 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 
-/**
- * _putchar - prints a character
- * @c: the character to be printed
- * Return: int
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
