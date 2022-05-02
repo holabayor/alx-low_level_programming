056da7533dfac3d34844a38c01e6f00d665fbc95
holabayor <liasu.olabayo@gmail.com>
2022-05-02 22:42:08 +0100

    fixed task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - reads a text file and prints it to the POSIX standard output
+ * create_file - creates a file
  * @filename: file to be read
  * @text_content: the text content
  * Return: 1 on success, -1 on failure
@@ -9,18 +9,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd, len = 0;
+	ssize_t nr, nw;
 
-	int fd, i = 0;
+	if (filename == NULL)
+		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i] != '\0')
-		i++;
+	if (text_content == NULL)
+		text_content = "";
 
-	write(fd, text_content, i);
+	while (text_content[len] != '\0')
+		len++;
+
+	write(STDOUT_FILENO, text_content, len);
+
 	close(fd);
 
 	return (1);
