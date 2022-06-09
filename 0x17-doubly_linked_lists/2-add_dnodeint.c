4267e714c9f8f89f0bdeda483842573870da05a7
holabayor <liasu.olabayo@gmail.com>
2022-06-09 06:39:13 -0700

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - prints the length of a list
+ * @head: head of the list
+ * @n: value of the new node
+ * Return: the number of nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
