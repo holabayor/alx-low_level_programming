59df02002bb01512792d8814e4f53dfbd2072da1
holabayor <liasu.olabayo@gmail.com>
2022-06-09 20:00:06 +0100

    add node at end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,6 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
         while (current->next != NULL)
             current = current->next;
         current->next = new;
+        new->prev = current;
     }
 	return (*head);
 }
