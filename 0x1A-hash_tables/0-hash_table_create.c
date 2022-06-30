9725670ffc94a4d8dd02060bea6b21a4dc5e5a8f
holabayor <liasu.olabayo@gmail.com>
2022-06-30 18:21:54 +0100

    fixed malloc in hash array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,11 +7,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *table;
 
 
-	table = malloc(sizeof(hash_table_t) * size);
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
+
+	table->array = malloc(sizeof(hash_node_t) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+
 	return (table);
 }
