1be4129f853fabae45b14f98b196789ec6a61bd2
holabayor <liasu.olabayo@gmail.com>
2022-10-04 11:23:22 +0100

    testing mic

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -21,7 +21,6 @@ int interpolation_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 	while ((array[high] != array[low]))
-	       /*	&& (value >= array[low]) && (value <= array[high])) */
 	{
 		pos = low + (((double)(high - low) /
 					(array[high] - array[low])) * (value - array[low]));
